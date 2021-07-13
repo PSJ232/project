0b688a94e8d692ded284c917a93e1cbb6e3ed213
ITWILL <ITWILL@DESKTOP-HCCQGGL>
2021-07-13 18:25:19 +0900

    memberjoin

---
+++ Project/src/main/java/vo/MemberBean.java
@@ -0,0 +1,19 @@
+package vo;
+
+import java.sql.Timestamp;
+
+public class MemberBean {
+	private String m_id;
+	private String m_pass;
+	private String m_name;
+	private String m_phone;
+	private String m_birth;
+	private int m_gender;
+	private int g_id;
+	private int m_agree;
+	private String m_address;
+	private Timestamp m_rdate;
+	private String m_drop;
+	
+	//데이터타입 확인 필요 *신종현
+}
