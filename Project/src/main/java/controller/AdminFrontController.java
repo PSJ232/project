1ff6cf85bb18b071a95637e437b053c7c85af9f6
DESKTOP-HCCQGGL\ITWILL <wkwmd7@gmail.com>
2021-07-16 16:50:36 +0900

    override doGet, doPost

--- Project/src/main/java/controller/AdminFrontController.java
+++ Project/src/main/java/controller/AdminFrontController.java
@@ -135,14 +135,17 @@ public class AdminFrontController extends HttpServlet {
 
 		}
 	}
-       
 
+	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doProcess(request, response);
 	}
-	
-	protected void doPOST(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doProcess(request, response);
 	}
+       
+	
 
 }
